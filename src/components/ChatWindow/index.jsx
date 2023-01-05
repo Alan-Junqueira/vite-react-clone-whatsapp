10e5f7cba8fe3d6fbfd80604df6b2e2428371830
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-01-05 14:31:05 -0300

    configure emojiPicker

--- src/components/ChatWindow/index.jsx
+++ src/components/ChatWindow/index.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+import EmojiPicker from 'emoji-picker-react';
+
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import CloseIcon from '@mui/icons-material/Close';
+import SendIcon from '@mui/icons-material/Send';
+import MicIcon from '@mui/icons-material/Mic';
 
 import avatarImage from '../../assets/avatar.png';
 
 import './styles.css';
 
 export const ChatWindow = () => {
+  const [emojiOpen, setEmojiOpen] = useState(false);
+  const [text, setText] = useState('');
+
+  function handleEmojiClick(e) {
+    console.log(e.emoji);
+    setText(text + e.emoji);
+  }
   return (
     <div className="chatWindow">
       <div className="chatWindow--header">
@@ -16,21 +30,59 @@ export const ChatWindow = () => {
         </div>
         <div className="chatWindow--header-buttons">
           <button className="chatWindow--button">
-            <SearchIcon fontSize='large'/>
+            <SearchIcon fontSize="large" />
           </button>
           <button className="chatWindow--button">
-            <AttachFileIcon fontSize='large'/>
+            <AttachFileIcon fontSize="large" />
           </button>
           <button className="chatWindow--button">
-            <MoreVertIcon fontSize='large'/>
+            <MoreVertIcon fontSize="large" />
           </button>
         </div>
       </div>
-      <div className="chatWindow--body">
-
+      <div className="chatWindow--body"></div>
+      <div
+        className="chatWindow--emoji-area"
+        style={{ height: emojiOpen ? '20rem' : '0' }}
+      >
+        <EmojiPicker
+          searchDisabled
+          skinTonesDisabled
+          onEmojiClick={handleEmojiClick}
+          previewConfig={{ showPreview: false }}
+        />
       </div>
       <div className="chatWindow--footer">
-        
+        <div className="chatWindow--pre">
+          <button
+            className="chatWindow--button"
+            onClick={() => setEmojiOpen(false)}
+            style={{ width: emojiOpen ? 40 : 0 }}
+          >
+            <CloseIcon fontSize="large" />
+          </button>
+          <button className="chatWindow--button">
+            <InsertEmoticonIcon
+              fontSize="large"
+              onClick={() => setEmojiOpen(true)}
+              style={{ color: emojiOpen && '#009688' }}
+            />
+          </button>
+        </div>
+        <div className="chatWindow--input-area">
+          <input
+            type="text"
+            className="chatWindow-input"
+            placeholder="Digite uma mensagem"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="chatWindow--pos">
+          <button className="chatWindow--button">
+            <SendIcon fontSize="large" />
+          </button>
+        </div>
       </div>
     </div>
   );
